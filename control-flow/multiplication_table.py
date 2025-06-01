11100266c2b7758e5cff33bd0cf96d3f33b2a374
Loisina <Adjoaasiedu4@gmail.com>
2025-06-01 23:37:43 +0000

    testing conditional statements

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for i in range(1, 11):
     result = number * i
-    print(f"{number} x {i} = {result}")
+    print(f"{number} * {i} = {result}")
 
     
