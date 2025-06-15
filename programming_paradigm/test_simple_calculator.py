a61933f1bf53cfa287b56d1024625f5dac0e97d0
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-06-15 23:30:21 +0000

    week 6

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Create an instance of SimpleCalculator before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(0, 0), 0)
