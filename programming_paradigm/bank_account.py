e65a92c4af0f7d528d600237188bbb17367bdab2
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-06-15 23:09:45 +0000

    week 6

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,16 +5,16 @@ class BankAccount:
   def deposit(self,amount):
     if amount > 0:
       self.account_balance += amount
-      print (f"Deposited: ${amount:.1f}")
+      print (f"Deposited: ${amount}")
 
   def withdraw(self,amount):
      if amount <= self.account_balance:
         self.account_balance -= amount
-        print(f"Withdrew: ${amount:.2f}")
+        print(f"Withdrew: ${amount}")
         return True
      else:
         print("Insufficient funds. Withdrawal denied.")
         return False
      
   def display_balance(self):
-    print(f"Current Balance: ${self.account_balance:.2f}")
+    print(f"Current Balance: ${self.account_balance}")
